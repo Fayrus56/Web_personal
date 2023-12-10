5e5c68c184688925ee14b12c0b0eb546a9910663
Audrey Yusuf F <130260611+Fayrus56@users.noreply.github.com>
2023-12-10 10:09:05 +0700

    Add files via upload
    sedikit perbaikan typo

---
+++ boostrap/main.js
@@ -0,0 +1,11 @@
+
+//  ini untuk ke ig ku
+function tIG() {
+    window.open('https://www.instagram.com/ysssyf_fyrs/', '_blank');
+}
+
+// ini untuk ke no wa
+function tWA() {
+    const noWa = '6287755507938'
+    window.open('https://wa.me/' + noWa, '_blank')
+}
