50c9331348e21c7025853e7df1e24fb5828ff3de
Audrey Yusuf F <130260611+Fayrus56@users.noreply.github.com>
2023-12-14 19:10:48 +0700

    Add files via upload
    version 1.2

--- boostrap/main.js
+++ boostrap/main.js
@@ -9,3 +9,8 @@ function tWA() {
     const noWa = '6287755507938'
     window.open('https://wa.me/' + noWa, '_blank')
 }
+
+// ini terhubung ke tombol tang mengambang
+function wa() {
+    window.open('https://wa.me/628975828050','_blank')
+}
